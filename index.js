d4fab2bc8fd45ae242487d8ae994e3ab470e9eb7
stephen owino <161324981+Owinot@users.noreply.github.com>
2024-03-22 05:24:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser="Bobby"
+const welcomeMessage=' Welcome to Flatbook ${currentUser}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreating= `Welcome ${Grace .slice(0,1)}!`
